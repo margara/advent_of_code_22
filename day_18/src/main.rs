30b181dea967785432c8666c508fd2971ca7b155
Alessandro Margara <alessandro.margara@polimi.it>
2023-01-08 15:59:12 +0100

    Day 18 (second part)

--- day_18/src/main.rs
+++ day_18/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::{self, BufRead};
 use adjacent_pair_iterator::AdjacentPairIterator;
@@ -15,47 +16,29 @@ fn parse_input() -> Vec<(i32, i32, i32)> {
     }).collect()
 }
 
-fn main() {
-    let mut input = parse_input();
+fn connected(input: &mut Vec<(i32, i32, i32)>,
+             key: fn((i32, i32, i32)) -> (i32, i32),
+             val: fn((i32, i32, i32)) -> i32)
+    -> usize {
 
-    input.sort_by(|(x1, y1, z1), (x2, y2, z2)| (y1, z1).cmp(&(y2, z2)));
-    let connected_x = input.iter()
-        .group_by(|(_x, y, z)| (y, z))
+    input.sort_by(|p1, p2| key(*p1).cmp(&key(*p2)));
+    input.iter()
+        .group_by(|&p| key(*p))
         .into_iter()
         .map(|(_key, points)| points
             .into_iter()
-            .map(|(x, _y, _z)| x)
+            .map(|&p| val(p))
             .sorted()
             .adjacent_pairs()
-            .filter(|(&x1, &x2)| x1.abs_diff(x2) == 1)
+            .filter(|(v1, v2)| v1.abs_diff(*v2) == 1)
             .count() * 2)
-        .sum::<usize>();
+        .sum::<usize>()
+}
 
-    input.sort_by(|(x1, y1, z1), (x2, y2, z2)| (x1, z1).cmp(&(x2, z2)));
-    let connected_y = input.iter()
-        .group_by(|(x, _y, z)| (x, z))
-        .into_iter()
-        .map(|(_key, points)| points
-            .into_iter()
-            .map(|(_x, y, _z)| y)
-            .sorted()
-            .adjacent_pairs()
-            .filter(|(&y1, &y2)| y1.abs_diff(y2) == 1)
-            .count() * 2)
-        .sum::<usize>();
-
-    input.sort_by(|(x1, y1, z1), (x2, y2, z2)| (x1, y1).cmp(&(x2, y2)));
-    let connected_z = input.iter()
-        .group_by(|(x, y, _z)| (x, y))
-        .into_iter()
-        .map(|(_key, points)| points
-            .into_iter()
-            .map(|(_x, _y, z)| z)
-            .sorted()
-            .adjacent_pairs()
-            .filter(|(&z1, &z2)| z1.abs_diff(z2) == 1)
-            .count() * 2)
-        .sum::<usize>();
+fn compute_connected(mut input: &mut Vec<(i32, i32, i32)>) {
+    let connected_x = connected(&mut input, |(_x, y, z)| (y, z), |(x, _y, _z)| x);
+    let connected_y = connected(&mut input, |(x, _y, z)| (x, z), |(_x, y, _z)| y);
+    let connected_z = connected(&mut input, |(x, y, _z)| (x, y), |(_x, _y, z)| z);
 
     let tot_faces = input.len() * 6;
     let connected = connected_x + connected_y + connected_z;
@@ -65,6 +48,69 @@ fn main() {
     println!("Faces connected over y: {}", connected_y);
     println!("Faces connected over z: {}", connected_z);
     println!("Total number of faces: {}. Connected: {}. Not connected: {}", tot_faces, connected, not_connected);
-
-
 }
+
+fn reachable_from(input: &HashSet<(i32, i32, i32)>, current: &HashSet<(i32, i32, i32)>, all: &mut HashSet<(i32, i32, i32)>, max_x: i32, max_y: i32, max_z: i32) {
+    fn check_reachable(input: &HashSet<(i32, i32, i32)>, current: &HashSet<(i32, i32, i32)>, all: &mut HashSet<(i32, i32, i32)>, new: &mut HashSet<(i32, i32, i32)>, r: &(i32, i32, i32)) {
+        if !all.contains(r) && !current.contains(r) && !input.contains(r) {
+            new.insert(*r);
+            all.insert(*r);
+        }
+    }
+
+    let mut new = HashSet::new();
+    current.iter().for_each(|(x, y, z)| {
+        if *x > 0 { check_reachable(input, current, all, &mut new, &(*x-1, *y, *z)) }
+        if *y > 0 { check_reachable(input, current, all, &mut new, &(*x, *y-1, *z)) }
+        if *z > 0 { check_reachable(input, current, all, &mut new, &(*x, *y, *z-1)) }
+        if *x < max_x { check_reachable(input, current, all, &mut new, &(*x+1, *y, *z)) }
+        if *y < max_y { check_reachable(input, current, all, &mut new, &(*x, *y+1, *z)) }
+        if *z < max_z { check_reachable(input, current, all, &mut new, &(*x, *y, *z+1)) }
+    });
+
+    if !new.is_empty() {
+        reachable_from(input, &mut new, all, max_x, max_y, max_z);
+    }
+}
+
+fn compute_reachable(input: &HashSet<(i32, i32, i32)>) -> HashSet<(i32, i32, i32)> {
+    let current = HashSet::from([(0, 0, 0)]);
+    let mut reachable = HashSet::new();
+    let max_x = input.iter().map(|(x, _y, _z)| *x).max().unwrap() + 1;
+    let max_y = input.iter().map(|(_x, y, _z)| *y).max().unwrap() + 1;
+    let max_z = input.iter().map(|(_x, _y, z)| *z).max().unwrap() + 1;
+    reachable_from(input, &current, &mut reachable, max_x, max_y, max_z);
+    reachable
+}
+
+fn all(input: &HashSet<(i32, i32, i32)>) -> HashSet<(i32, i32, i32)> {
+    let max_x = input.iter().map(|(x, _y, _z)| *x).max().unwrap() + 1;
+    let max_y = input.iter().map(|(_x, y, _z)| *y).max().unwrap() + 1;
+    let max_z = input.iter().map(|(_x, _y, z)| *z).max().unwrap() + 1;
+    let mut res = HashSet::new();
+    for x in 0..max_x {
+        for y in 0..max_y {
+            for z in 0..max_z {
+                res.insert((x, y, z));
+            }
+        }
+    }
+    res
+}
+
+fn main() {
+    let mut input = parse_input();
+    let mut input_set = input.iter().cloned().collect::<HashSet<_>>();
+
+    // Part 1
+    compute_connected(&mut input);
+
+    // Part 2
+    let all = all(&input_set);
+    let reachable = compute_reachable(&input_set);
+    let unreachable = all.difference(&reachable).cloned().collect::<HashSet<_>>();
+    let mut input_with_unreachable = input_set.union(&unreachable).cloned().collect::<Vec<_>>();
+    compute_connected(&mut input_with_unreachable);
+}
+
+
